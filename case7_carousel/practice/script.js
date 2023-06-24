d6fde4afc1eff95bd70258bd64e95b2d55412e90
chaeeunj <official2930@naver.com>
2023-06-25 02:55:39 +0900

    case7 carousel 슬라이더 동작하는 동안 네비 기능 막기 구현

--- case7_carousel/practice/script.js
+++ case7_carousel/practice/script.js
@@ -13,12 +13,21 @@
 
       this.totalItems = this.items.length // 5
       this.current = 0
+      this.isMoving = false
     }
 
     initCarousel() {
+      this.isMoving = false
       this.items[0].classList.add('active')
       this.items[1].classList.add('next')
       this.items[this.totalItems - 1].classList.add('prev')
+    }
+
+    disabledInteraction() {
+      this.isMoving = true
+      setTimeout(() => {
+        this.isMoving = false
+      }, 500)
     }
 
     setEventListener() {
@@ -38,6 +47,8 @@
     }
 
     moveCarouselTo() {
+      if (this.isMoving) return
+      this.disabledInteraction()
       let prev = this.current - 1
       let next = this.current + 1
 
@@ -61,6 +72,7 @@
     }
 
     moveNext() {
+      if (this.isMoving) return
       if (this.current === this.totalItems - 1) {
         this.current = 0
       } else {
@@ -70,6 +82,7 @@
     }
 
     movePrev() {
+      if (this.isMoving) return
       if (this.current === 0) {
         this.current = this.totalItems - 1
       } else {

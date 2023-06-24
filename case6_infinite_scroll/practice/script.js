df7abf04dff030a83d1903cf8219f47ad447df7f
chaeeunj <official2930@naver.com>
2023-06-24 18:21:25 +0900

    페이지 나누기 & 로딩 엘리먼트 구현

--- case6_infinite_scroll/practice/script.js
+++ case6_infinite_scroll/practice/script.js
@@ -5,10 +5,16 @@
     return document.querySelector(target)
   }
 
+  let page = 1
+  const limit = 10
   const $posts = get('.posts')
+  const end = 100
+  let total = 10
+
+  const $loader = get('.loader')
 
   const getPost = async () => {
-    const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
     const response = await fetch(API_URL)
     if (!response.ok) {
       throw new Error('에러가 발생했습니다.')
@@ -33,12 +39,44 @@
     })
   }
 
+  const showLoader = () => {
+    $loader.classList.add('show')
+  }
+
+  const hideLoader = () => {
+    $loader.classList.remove('show')
+  }
+
   const loadPost = async () => {
-    const response = await getPost()
-    showPosts(response)
+    // 로딩 엘리먼트를 보여줌
+    showLoader()
+    try {
+      const response = await getPost()
+      showPosts(response)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      // 로딩 엘리먼트를 사라지게 함
+      hideLoader()
+    }
+  }
+
+  const onScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+
+    if (total === end) {
+      window.removeEventListener('scroll', onScroll)
+      return
+    }
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      page++
+      total += 10
+      loadPost()
+    }
   }
 
   window.addEventListener('DOMContentLoaded', () => {
     loadPost()
+    window.addEventListener('scroll', onScroll)
   })
 })()
